5aef5bd78058d745a7d9842d47f7fe6f61e52471
Ioannis Charitopoulos <icharitopoulos@gmail.com>
2021-05-19 12:20:28 +0300

    Added test for consistent item names

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -2,7 +2,7 @@
 import unittest
 
 from gilded_rose import Item, GildedRose
-
+from texttest_fixture import item_set
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
@@ -10,6 +10,17 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals("foo", items[0].name)
+
+    def test_name_remains_same(self):
+        # set of items tested
+        items = item_set
+        # expected item set output
+        expected_items = item_set
+
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        for i in range(len(items)):
+            self.assertEquals(expected_items[i].name, items[i].name)
 
         
 if __name__ == '__main__':
