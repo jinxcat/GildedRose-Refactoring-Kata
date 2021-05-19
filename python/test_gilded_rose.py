3bb90ec907a2a24c055ba8cd43eacbad07616964
Ioannis Charitopoulos <icharitopoulos@gmail.com>
2021-05-19 13:31:29 +0300

    Implemented attribute decrease test, skipped unimplemented tests

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -23,9 +23,26 @@ class GildedRoseTest(unittest.TestCase):
             self.assertEquals(expected_items[i].name, items[i].name)
 
     def test_expiration_and_quality_drop_for_normal(self):
-        pass
+        # set of items tested
+        items = [
+            Item("+5 Dexterity Vest", 10, 20),
+            Item("+5 Dexterity Vest", 5, 10)
+        ]
+        # expected item set output
+        expected = [
+            {'sell_in': 9, 'quality': 19},
+            {'sell_in': 4, 'quality': 9},
+        ]
+
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        for index, expectation in enumerate(expected):
+            item = items[index]
+            self.assertEqual(item.quality, expectation['quality'])
+            self.assertEqual(item.sell_in, expectation['sell_in'])
 
     def test_quality_never_becomes_negative(self):
+        self.skipTest("not implemented yet")
         pass
 
     def test_quality_drops_twice_as_fast_for_expired_items(self):
@@ -48,24 +65,31 @@ class GildedRoseTest(unittest.TestCase):
             self.assertEqual(item.sell_in, expectation['sell_in'])
 
     def test_aged_brie_increases_quality_as_time_passes(self):
+        self.skipTest("not implemented yet")
         pass
 
     def test_quality_is_never_above_50(self):
+        self.skipTest("not implemented yet")
         pass
 
     def test_sulfuras_never_decreases_in_quality(self):
+        self.skipTest("not implemented yet")
         pass
 
     def test_backstage_passes_increase_by_2_for_10_days_to_event(self):
+        self.skipTest("not implemented yet")
         pass
 
     def test_backstage_passes_increase_by_3_for_5_days_to_event(self):
+        self.skipTest("not implemented yet")
         pass
 
     def test_backstage_passes_to_zero_when_expired(self):
+        self.skipTest("not implemented yet")
         pass
 
     def test_conjured_items_decrease_twice_as_fast_as_normal(self):
+        self.skipTest("not implemented yet")
         pass
 
         
